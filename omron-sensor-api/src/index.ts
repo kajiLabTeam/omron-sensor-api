4825e1db3533f0b35973d97796308dabf9f7139e
harutiro <harutiro2027@outlook.jp>
2024-04-09 10:55:53 +0900

    fix: ディレクトリ名を間違えていた問題を解決

--- omron-sensor-api/src/index.ts
+++ omron-sensor-api/src/index.ts
@@ -1,7 +1,7 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import { logger } from 'hono/logger'
-import { getAllArea, getArea, setArea } from './features/area/Usecaes/AreaUsecase'
+import { getAllArea, getArea, setArea } from './features/area/Usecase/AreaUsecase'
 import { getSensor, setSensor } from './features/sensor/Usecase/SensorUsecase'
 
 
